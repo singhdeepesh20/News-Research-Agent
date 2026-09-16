265807a02cdab90b55e63b104f8eb3bf8d9055c8
Deepesh singh <mr.deepesh.singh.20@gmail.com>
2026-09-16 23:51:05 +0530

    Add stop execution if API keys are missing
    Stop execution if API keys are not provided.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ HF_TOKEN = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 
 if not GROQ_API_KEY or not HF_TOKEN:
     st.error("Please enter GROQ_API_KEY and HUGGINGFACEHUB_API_TOKEN in your .env file.")
-    
+    st.stop()
 
 
 st.set_page_config(
